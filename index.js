8923629e9499cf01e089f164cb91405245fb6c8b
Olav017 <00001105977250sp@al.educacao.sp.gov.br>
2024-10-17 13:49:53 -0300

    mudanças na aparencia

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const avanca = document. querySelectorAll(".btn-proximo")
 
 avanca.forEach(button => {
-    button.addEventListener("clik", function())
+    button.addEventListener("clik", function(){
+     const atual = document,querySelector("ativo");
+     const proximoPasso = "passo"+ this.getAttribute("data-proximo");
+     atual.classList.reove("ativo");
+     document.getElementById(proximoPasso).classList.add("ativo")
+   })
 })
